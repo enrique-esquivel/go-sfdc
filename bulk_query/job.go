41d6ef7bab484a78117b942ccad70d7deb028141
Felipe Crochik <felipe@crochik.com>
2021-01-30 11:01:00 -0500

    queryjob

---
+++ bulk_query/job.go
@@ -0,0 +1,338 @@
+package bulkquery
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/crochik/go-sfdc"
+	"github.com/crochik/go-sfdc/session"
+)
+
+// QueryJobType is the bulk job type.
+type QueryJobType string
+
+const (
+	// V2Query is the big objects job.
+	V2Query QueryJobType = "V2Query"
+)
+
+// ColumnDelimiter is the column delimiter used for CSV job data.
+type ColumnDelimiter string
+
+const (
+	// Backquote is the (`) character.
+	Backquote ColumnDelimiter = "BACKQUOTE"
+	// Caret is the (^) character.
+	Caret ColumnDelimiter = "CARET"
+	// Comma is the (,) character.
+	Comma ColumnDelimiter = "COMMA"
+	// Pipe is the (|) character.
+	Pipe ColumnDelimiter = "PIPE"
+	// SemiColon is the (;) character.
+	SemiColon ColumnDelimiter = "SEMICOLON"
+	// Tab is the (\t) character.
+	Tab ColumnDelimiter = "TAB"
+)
+
+// ContentType is the format of the data being processed.
+type ContentType string
+
+// CSV is the supported content data type.
+const CSV ContentType = "CSV"
+
+// LineEnding is the line ending used for the CSV job data.
+type LineEnding string
+
+const (
+	// Linefeed is the (\n) character.
+	Linefeed LineEnding = "LF"
+	// CarriageReturnLinefeed is the (\r\n) character.
+	CarriageReturnLinefeed LineEnding = "CRLF"
+)
+
+// QueryOperation is the processing operation for the job.
+type QueryOperation string
+
+const (
+	// Query Returns data that has not been deleted or archived
+	Query QueryOperation = "query"
+	// QueryAll Returns records that have been deleted because of a merge or delete, and returns information about archived Task and Event records
+	QueryAll QueryOperation = "queryAll"
+)
+
+// State is the current state of processing for the job.
+type State string
+
+const (
+	// Open the job has been created and job data can be uploaded tothe job.
+	Open State = "Open"
+	// UpdateComplete all data for the job has been uploaded and the job is ready to be queued and processed.
+	UpdateComplete State = "UploadComplete"
+	// Aborted the job has been aborted.
+	Aborted State = "Aborted"
+	// JobComplete the job was processed by Salesforce.
+	JobComplete State = "JobComplete"
+	// Failed some records in the job failed.
+	Failed State = "Failed"
+)
+
+const (
+	// sfID is the column name for the Salesforce Object ID in Job CSV responses
+	sfID = "sf__Id"
+
+	// sfError is the column name for the error in Failed record responses
+	sfError = "sf__Error"
+
+	// sfError is the column name for the created flag in Successful record responses
+	sfCreated = "sf__Created"
+)
+
+// QueryOptions are the options for the job.
+//
+// ColumnDelimiter is the delimiter used for the CSV job.  This field is optional.
+//
+// ContentType is the content type for the job.  This field is optional.
+//
+// LineEnding is the line ending used for the CSV job data.  This field is optional.
+//
+// QueryOptions is the processing operation for the job. This field is required.
+type QueryOptions struct {
+	ColumnDelimiter ColumnDelimiter `json:"columnDelimiter"`
+	ContentType     ContentType     `json:"contentType"`
+	LineEnding      LineEnding      `json:"lineEnding"`
+	Query           string          `json:"query"`
+	Operation       QueryOperation  `json:"operation"`
+}
+
+// QueryResponse is the response to job APIs.
+type QueryResponse struct {
+	APIVersion      float32         `json:"apiVersion"`
+	ColumnDelimiter ColumnDelimiter `json:"columnDelimiter"`
+	ConcurrencyMode string          `json:"concurrencyMode"`
+	ContentType     string          `json:"contentType"`
+	CreatedByID     string          `json:"createdById"`
+	CreatedDate     string          `json:"createdDate"`
+	ID              string          `json:"id"`
+	JobType         QueryJobType    `json:"jobType"`
+	LineEnding      LineEnding      `json:"lineEnding"`
+	Object          string          `json:"object"`
+	Operation       QueryOperation  `json:"operation"`
+	State           State           `json:"state"`
+	SystemModstamp  string          `json:"systemModstamp"`
+}
+
+// QueryInfo is the response to the job information API.
+type QueryInfo struct {
+	QueryResponse
+	// ApexProcessingTime      int    `json:"apexProcessingTime"`
+	// APIActiveProcessingTime int    `json:"apiActiveProcessingTime"`
+	// NumberRecordsFailed     int    `json:"numberRecordsFailed"`
+	NumberRecordsProcessed int `json:"numberRecordsProcessed"`
+	Retries                int `json:"retries"`
+	TotalProcessingTime    int `json:"totalProcessingTime"`
+	// ErrorMessage            string `json:"errorMessage"`
+}
+
+// QueryJob is the bulk job.
+type QueryJob struct {
+	session session.ServiceFormatter
+	info    QueryResponse
+}
+
+func (j *QueryJob) create(options QueryOptions) error {
+	err := j.formatOptions(&options)
+	if err != nil {
+		return err
+	}
+	j.info, err = j.createCallout(options)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (j *QueryJob) formatOptions(options *QueryOptions) error {
+	if options.Operation == "" {
+		return errors.New("bulk job: operation is required")
+	}
+	if options.Query == "" {
+		return errors.New("bulk job: query is required")
+	}
+	if options.LineEnding == "" {
+		options.LineEnding = Linefeed
+	}
+	if options.ContentType == "" {
+		options.ContentType = CSV
+	}
+	if options.ColumnDelimiter == "" {
+		options.ColumnDelimiter = Comma
+	}
+	return nil
+}
+
+func (j *QueryJob) createCallout(options QueryOptions) (QueryResponse, error) {
+	url := j.session.ServiceURL() + bulk2Endpoint
+	body, err := json.Marshal(options)
+	if err != nil {
+		return QueryResponse{}, err
+	}
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(body))
+	if err != nil {
+		return QueryResponse{}, err
+	}
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("Content-Type", "application/json")
+	j.session.AuthorizationHeader(request)
+
+	return j.response(request)
+}
+
+func (j *QueryJob) response(request *http.Request) (QueryResponse, error) {
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return QueryResponse{}, err
+	}
+
+	decoder := json.NewDecoder(response.Body)
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return QueryResponse{}, sfdc.HandleError(response)
+	}
+
+	var value QueryResponse
+	err = decoder.Decode(&value)
+	if err != nil {
+		return QueryResponse{}, err
+	}
+	return value, nil
+}
+
+// Info returns the current job information.
+func (j *QueryJob) Info() (QueryInfo, error) {
+	return j.fetchInfo(j.info.ID)
+}
+
+func (j *QueryJob) fetchInfo(id string) (QueryInfo, error) {
+	url := j.session.ServiceURL() + bulk2Endpoint + "/" + id
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return QueryInfo{}, err
+	}
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("Content-Type", "application/json")
+	j.session.AuthorizationHeader(request)
+
+	return j.infoResponse(request)
+}
+
+func (j *QueryJob) infoResponse(request *http.Request) (QueryInfo, error) {
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return QueryInfo{}, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		err := sfdc.HandleError(response)
+		return QueryInfo{}, err
+	}
+
+	decoder := json.NewDecoder(response.Body)
+	var value QueryInfo
+	err = decoder.Decode(&value)
+	if err != nil {
+		return QueryInfo{}, err
+	}
+	return value, nil
+}
+
+func (j *QueryJob) setState(state State) (QueryResponse, error) {
+	url := j.session.ServiceURL() + bulk2Endpoint + "/" + j.info.ID
+	jobState := struct {
+		State string `json:"state"`
+	}{
+		State: string(state),
+	}
+	body, err := json.Marshal(jobState)
+	if err != nil {
+		return QueryResponse{}, err
+	}
+	request, err := http.NewRequest(http.MethodPatch, url, bytes.NewReader(body))
+	if err != nil {
+		return QueryResponse{}, err
+	}
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("Content-Type", "application/json")
+	j.session.AuthorizationHeader(request)
+
+	return j.response(request)
+}
+
+// Abort will abort the current job.
+func (j *QueryJob) Abort() (QueryResponse, error) {
+	return j.setState(Aborted)
+}
+
+// Delete will delete the current job.
+func (j *QueryJob) Delete() error {
+	url := j.session.ServiceURL() + bulk2Endpoint + "/" + j.info.ID
+	request, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return err
+	}
+	j.session.AuthorizationHeader(request)
+
+	response, err := j.session.Client().Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusNoContent {
+		return errors.New("job error: unable to delete job")
+	}
+	return nil
+}
+
+func (j *QueryJob) headerPosition(column string, header []string) int {
+	for idx, col := range header {
+		if col == column {
+			return idx
+		}
+	}
+	return -1
+}
+
+func (j *QueryJob) fields(header []string, offset int) []string {
+	fields := make([]string, len(header)-offset)
+	copy(fields[:], header[offset:])
+	return fields
+}
+
+func (j *QueryJob) record(fields, values []string) map[string]string {
+	record := make(map[string]string)
+	for idx, field := range fields {
+		record[field] = values[idx]
+	}
+	return record
+}
+
+func (j *QueryJob) delimiter() rune {
+	switch ColumnDelimiter(j.info.ColumnDelimiter) {
+	case Tab:
+		return '\t'
+	case SemiColon:
+		return ';'
+	case Pipe:
+		return '|'
+	case Caret:
+		return '^'
+	case Backquote:
+		return '`'
+	default:
+		return ','
+	}
+}
---
+++ bulk_query/resource.go
@@ -0,0 +1,66 @@
+package bulkquery
+
+import (
+	"github.com/crochik/go-sfdc/session"
+	"github.com/pkg/errors"
+)
+
+const bulk2Endpoint = "/jobs/query"
+
+// Resource is the structure that can be used to create bulk 2.0 jobs.
+type Resource struct {
+	session session.ServiceFormatter
+}
+
+// NewResource creates a new bulk 2.0 REST resource.  If the session is nil
+// an error will be returned.
+func NewResource(session session.ServiceFormatter) (*Resource, error) {
+	if session == nil {
+		return nil, errors.New("bulk: session can not be nil")
+	}
+
+	err := session.Refresh()
+	if err != nil {
+		return nil, errors.Wrap(err, "session refresh")
+	}
+
+	return &Resource{
+		session: session,
+	}, nil
+}
+
+// CreateJob will create a new bulk 2.0 job from the options that where passed.
+// The Job that is returned can be used to upload object data to the Salesforce org.
+func (r *Resource) CreateJob(options QueryOptions) (*QueryJob, error) {
+	job := &QueryJob{
+		session: r.session,
+	}
+	if err := job.create(options); err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+// GetJob will retrieve an existing bulk 2.0 job using the provided ID.
+func (r *Resource) GetJob(id string) (*QueryJob, error) {
+	job := &QueryJob{
+		session: r.session,
+	}
+	info, err := job.fetchInfo(id)
+	if err != nil {
+		return nil, err
+	}
+	job.info = info.QueryResponse
+
+	return job, nil
+}
+
+// // AllJobs will retrieve all of the bulk 2.0 jobs.
+// func (r *Resource) AllJobs(parameters Parameters) (*Jobs, error) {
+// 	jobs, err := newJobs(r.session, parameters)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return jobs, nil
+// }
